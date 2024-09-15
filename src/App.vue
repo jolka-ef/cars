ca86069f7e06951de255a26c1ee4cbfa2c01fd8b
Jolka <firjola@gmail.com>
2024-09-15 20:57:59 +0200

    feature : add footer

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<script setup>
+import Footer from '@/components/Footer.vue';
+</script>
+<template>
+  <Footer />
+</template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="border-t border-white bg-signalblack px-sm">
+    <div
+      class="m-auto mx-auto flex max-w-xl justify-between py-6 font-roboto_condensed text-base text-white"
+    >
+      <p>Cars Spot</p>
+      <a class="cursor-pointer underline">Polityka prywatności</a>
+    </div>
+  </footer>
+</template>
+<script></script>
